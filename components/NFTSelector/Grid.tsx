703217690ee62d814705df9dc7f555f9d2801822
shiftysloth <no>
2023-06-01 00:33:33 +0100

    feat: load more button for nfts

--- components/NFTSelector/Grid.tsx
+++ components/NFTSelector/Grid.tsx
@@ -1,5 +1,5 @@
 import Image from "next/legacy/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import type { GridProps, Item } from "./types";
 
@@ -10,6 +10,16 @@ export const Grid: React.FC<GridProps> = ({
   selected,
   collectionName,
 }) => {
+  const [itemCount, setItemCount] = useState(10);
+
+  const handleLoadMore = () => {
+    setItemCount((prevCount) => prevCount + 10);
+  };
+
+  useEffect(() => {
+    setItemCount(10);
+  }, [collectionName]);
+
   const handleClick = (
     target: HTMLElement,
     item: Item,
@@ -40,78 +50,89 @@ export const Grid: React.FC<GridProps> = ({
     }
   };
 
-  return items ? (
-    <div className="w-full h-full flex flex-wrap gap-4 justify-center items-center mt-12 mb-6 max-w-7xl">
-      {items.map((item) => {
-        const { tokenId, image } = item;
-        const isSelected = !!selected.find((i) => i.tokenId === tokenId);
-        return (
-          <div
-            key={tokenId}
-            className={`cursor-pointer relative w-[150px] h-[150px] rounded-md overflow-hidden self-center justify-self-center bg-gray-800 outline outline-4 outline-offset-2 ${
-              isSelected ? "outline-pink-500" : " outline-transparent"
-            }`}
-          >
-            <div className="w-full h-full absolute bg-gray-700 animate-pulse flex justify-center items-center">
-              <svg
-                className="w-12 h-12 text-gray-200"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 640 512"
-              >
-                <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"></path>
-              </svg>
-            </div>
-            <Image
-              priority
-              width={150}
-              height={150}
-              layout="fixed"
-              src={image}
-              alt={`${collectionName} ${tokenId}`}
-              className="transition-transform duration-175 ease-out hover:scale-105 object-cover object-center w-full h-full"
-            />
-            <div
-              className="absolute top-0 left-0 w-full h-full flex justify-center items-center transition-opacity ease-out opacity-0 hover:opacity-100 hover:bg-slate-900/75"
-              onClick={(event) => {
-                handleClick(event.currentTarget, item, isSelected);
-              }}
-            >
-              {isSelected ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-12 h-12"
+  return (
+    <>
+      <div className="w-full h-full flex flex-wrap gap-4 justify-center items-center mt-12 mb-6 max-w-7xl">
+        {items
+          ? items.slice(0, itemCount).map((item) => {
+              const { tokenId, image } = item;
+              const isSelected = !!selected.find((i) => i.tokenId === tokenId);
+              return (
+                <div
+                  key={tokenId}
+                  className={`cursor-pointer relative w-[150px] h-[150px] rounded-md overflow-hidden self-center justify-self-center bg-gray-800 outline outline-4 outline-offset-2 ${
+                    isSelected ? "outline-pink-500" : " outline-transparent"
+                  }`}
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm3 10.5a.75.75 0 000-1.5H9a.75.75 0 000 1.5h6z"
-                    clipRule="evenodd"
+                  <div className="w-full h-full absolute bg-gray-700 animate-pulse flex justify-center items-center">
+                    <svg
+                      className="w-12 h-12 text-gray-200"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 640 512"
+                    >
+                      <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"></path>
+                    </svg>
+                  </div>
+                  <Image
+                    width={150}
+                    height={150}
+                    layout="fixed"
+                    src={image}
+                    alt={`${collectionName} ${tokenId}`}
+                    className="transition-transform duration-175 ease-out hover:scale-105 object-cover object-center w-full h-full"
                   />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-12 h-12"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              )}
-            </div>
-            <div className="absolute pointer-events-none bottom-0 right-0 rounded-tl bg-black bg-opacity-75 text-white text-sm text-center py-1 px-2">
-              #{tokenId}
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  ) : null;
+                  <div
+                    className="absolute top-0 left-0 w-full h-full flex justify-center items-center transition-opacity ease-out opacity-0 hover:opacity-100 hover:bg-slate-900/75"
+                    onClick={(event) => {
+                      handleClick(event.currentTarget, item, isSelected);
+                    }}
+                  >
+                    {isSelected ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-12 h-12"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm3 10.5a.75.75 0 000-1.5H9a.75.75 0 000 1.5h6z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-12 h-12"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    )}
+                  </div>
+                  <div className="absolute pointer-events-none bottom-0 right-0 rounded-tl bg-black bg-opacity-75 text-white text-sm text-center py-1 px-2">
+                    #{tokenId}
+                  </div>
+                </div>
+              );
+            })
+          : null}
+      </div>
+      {itemCount <= items?.length && (
+        <button
+          className="flex items-center text-lg px-3 py-2 mb-2 rounded-md border-solid border-violet border-2"
+          onClick={handleLoadMore}
+        >
+          Load More
+        </button>
+      )}
+    </>
+  );
 };
