a2d957ca1359ff09e34258f4d332f978724e58d8
shiftysloth <no>
2023-06-08 12:23:30 +0100

    style: fix prop warning

--- pages/holder.js
+++ pages/holder.js
@@ -1,12 +1,17 @@
 import Image from "next/image";
+
 import Stepper from "../components/Stepper";
 
 const HolderSection = () => {
   return (
     <section>
-      <a className="relative block w-full max-w-2xl px-5 md:mx-auto" href="https://discord.gg/7bwXQU77bC" target="_blank" rel="noreferrer">
+      <a
+        className="relative block w-full max-w-2xl px-5 md:mx-auto"
+        href="https://discord.gg/7bwXQU77bC"
+        target="_blank"
+        rel="noreferrer"
+      >
         <Image
-          responsive
           width={673}
           height={164}
           alt="Cosmos Ape Alliance Generation 2 minting soon. Join the Discord."
