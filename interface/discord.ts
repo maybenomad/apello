6bbc9b874246c77764af22a986de421b7a9470e7
Rocco Nicosia <rocco@protonradio.com>
2024-03-18 10:38:41 -0500

    Add temporary test URL for Discord redirect

--- interface/discord.ts
+++ interface/discord.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const AUTHORIZE_URL =
-  "https://discord.com/api/oauth2/authorize?client_id=990757313390465114&redirect_uri=https%3A%2F%2Fapello.xyz%2Fholder&response_type=token&scope=identify";
+const TEST_URL = "apello-git-feat-holder-verify-redux-ryzs-projects.vercel.app";
+const URL = "apello.xyz";
+
+const AUTHORIZE_URL = `https://discord.com/api/oauth2/authorize?client_id=990757313390465114&redirect_uri=https%3A%2F%2F${TEST_URL}%2Fholder&response_type=token&scope=identify`;
 
 export function authorize() {
   window.open(AUTHORIZE_URL, "_self");
