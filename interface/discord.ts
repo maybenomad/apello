265395305e2adb2237e45aabc09dc6b404cff4fd
Rocco Nicosia <rocco@protonradio.com>
2024-03-21 20:30:05 -0500

    Replace test URL with normal Discord URL

--- interface/discord.ts
+++ interface/discord.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const TEST_URL = "apello-git-feat-holder-verify-redux-ryzs-projects.vercel.app";
 const URL = "apello.xyz";
 
-const AUTHORIZE_URL = `https://discord.com/api/oauth2/authorize?client_id=990757313390465114&redirect_uri=https%3A%2F%2F${TEST_URL}%2Fholder&response_type=token&scope=identify`;
+const AUTHORIZE_URL = `https://discord.com/api/oauth2/authorize?client_id=990757313390465114&redirect_uri=https%3A%2F%2F${URL}%2Fholder&response_type=token&scope=identify`;
 
 export function authorize() {
   window.open(AUTHORIZE_URL, "_self");
