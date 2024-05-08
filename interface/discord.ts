746f5a3feff494499b59494122d7afa3ec9e0f74
JoeVenner <ylafrimi@gmail.com>
2024-05-08 17:29:59 +0100

    Update discord hook domain

--- interface/discord.ts
+++ interface/discord.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "apello.xyz";
+const URL = "apello.org";
 
 const AUTHORIZE_URL = `https://discord.com/api/oauth2/authorize?client_id=990757313390465114&redirect_uri=https%3A%2F%2F${URL}%2Fholder&response_type=token&scope=identify`;
 
