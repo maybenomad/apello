f20d6bfc6f8784405174e5b24939a2a75f7aca46
Rocco Nicosia <rocco@protonradio.com>
2024-05-15 18:32:50 -0500

    fix: replace www subdomain when generating Discord URL

--- interface/discord.ts
+++ interface/discord.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const redirectURL = () => {
-  let origin = encodeURIComponent(window.location.origin);
+  let origin = encodeURIComponent(window.location.origin.replace("www.", ""));
   return `https://discord.com/api/oauth2/authorize?client_id=990757313390465114&redirect_uri=${origin}%2Fholder&response_type=token&scope=identify`;
 };
 
